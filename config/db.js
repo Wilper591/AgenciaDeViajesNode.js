a6751d16b19e54410aea2874698cca3d09042b94
Jorge Cerda <jorge_cerda_591@hotmail.com>
2024-05-26 01:53:30 -0400

    Restaurando configuracion mysql

--- config/db.js
+++ config/db.js
@@ -1,26 +1,18 @@
 const Sequelize = require("sequelize");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 dotenv.config();
 
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.HOST_DB,
-    port: "5432",
-    dialect: "postgres",
-    define: {
-      timestamps: false,
-    },
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-    operatorAliases: false,
-  }
-);
+const db = new Sequelize(process.env.DATABASE_URL, {
+  define: {
+    timestamps: false,
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  operatorAliases: false,
+});
 
 module.exports = db;
